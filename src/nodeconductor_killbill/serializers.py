13065800f3d93b5ed4bc46e9d73b215906376795
Roman Kosenko <Roman.Kosenko@gmail.com>
2015-11-23 11:34:46 +0000

    Another typo fix

--- src/nodeconductor_killbill/serializers.py
+++ src/nodeconductor_killbill/serializers.py
@@ -11,8 +11,8 @@ class InvoiceSerializer(core_serializers.AugmentedSerializerMixin,
     year = serializers.DateField(format='%Y', source='date')
     month = serializers.DateField(format='%m', source='date')
     customer_native_name = serializers.ReadOnlyField(source='customer.native_name')
-    pdf = serializers.HyperlinkedIdentityField(view_name='killbill-invoice-pdf', lookup_field='uuid')
-    usage_pdf = serializers.HyperlinkedIdentityField(view_name='killbill-invoice-usage-pdf', lookup_field='uuid')
+    pdf = serializers.HyperlinkedIdentityField(view_name='invoice-pdf', lookup_field='uuid')
+    usage_pdf = serializers.HyperlinkedIdentityField(view_name='invoice-usage-pdf', lookup_field='uuid')
 
     class Meta(object):
         model = Invoice
